1d2521a57c3b10bc34653363fc0e98b4d066848e
Tyler Hawkins <thawkin3@byu.net>
2023-04-16 23:30:33 -0600

    feat: styles the NoMatch page

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+import Paper from '@mui/material/Paper'
+import Typography from '@mui/material/Typography'
+import { makeStyles } from '@mui/styles'
+
+const useStyles = makeStyles(() => ({
+  root: {
+    padding: 32,
+  },
+}))
 
 export default function NoMatch() {
+  const classes = useStyles()
+
   return (
-    <div>
-      <h1>Nothing to see here!</h1>
-      <p>
-        <Link to="/">Go to the home page</Link>
-      </p>
-    </div>
+    <Container>
+      <Paper className={classes.root}>
+        <Typography component="h1" variant="h3" paragraph>
+          Nothing to see here!
+        </Typography>
+        <Typography paragraph>This page does not exist.</Typography>
+        <Button component={Link} to="/" variant="contained">
+          Go Back Home
+        </Button>
+      </Paper>
+    </Container>
   )
 }
